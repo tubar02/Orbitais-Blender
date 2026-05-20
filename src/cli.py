f85b143a1ef30821fc90f0f3263009aa6dfb9cb8
tubar02 <toszerodois@gmail.com>
2026-05-20 00:27:11 -0300

    Coloca um entry point em src

---
+++ src/cli.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+import src.grid.space as sp
+import src.io.export as xp
+import src.physics.atomic_orbitals as orb
+import src.physics.density as den
+import src.utils.plot as plt
+
+def main():
+	n, l, m = map(int, input("Digite os números quânticos n, l e m (separados por espaço): ").split())
+
+	assert n > 0, "n deve ser um inteiro positivo"
+	assert 0 <= l < n, "l deve ser um inteiro tal que 0 <= l < n"
+	assert -l <= m <= l, "m deve ser um inteiro tal que -l <= m <= l"
+
+	space = sp.Space()
+	wavefunction = orb.hydrogen_wavefunction(n, l, m, space)
+	density = den.probability_density(wavefunction, real=True, m=m)
+	percent = 0.01 * np.max(density)
+	mask = density >= percent
+
+	print("Deseja plotar a função de onda? (s/n)")
+	if input().lower() == 's':
+		mode = int(input("\nEscolha o modo de plotagem\n1: Scatter 3D\n2: Fatias 2D\n3: Isosuperfície\nDigite o número do modo: "))
+		if mode == 1:
+			plt.scatter3D(space, density)
+		elif mode == 2:
+			plt.slice_view(space, density)
+		elif mode == 3:
+			plt.scatter_masked(space, mask)
+	
+	print("Deseja salvar a função de onda em um arquivo? (s/n)")
+	if input().lower() == 's':
+		mode = int(input("\nEscolha o modo\n1: Isossuperfície única\n2: Variação da porcentagem\nDigite o número do modo: "))
+		if mode == 1:
+			xp.save_obj(space, density, f"orbital_n{n}_l{l}_m{m}", level=percent)
+		elif mode == 2:
+			for i in range(10):
+				xp.save_obj(space, density, f"orbital_n{n}_l{l}_m{m}", level=percent, mode=mode)
+				percent += 0.1 * np.max(density)
+
+if __name__ == '__main__':
+	main()
--- src/physics/gera_orbitais.py
+++ src/physics/gera_orbitais.py
@@ -8,44 +8,6 @@ from skimage.measure import marching_cubes # Para extração de isosuperfícies
 
 import src.io.paths as io
 
-def main():
-	n, l, m = map(int, input("Digite os números quânticos n, l e m (separados por espaço): ").split())
-
-	assert n > 0, "n deve ser um inteiro positivo"
-	assert 0 <= l < n, "l deve ser um inteiro tal que 0 <= l < n"
-	assert -l <= m <= l, "m deve ser um inteiro tal que -l <= m <= l"
-
-	atualiza_espaco(n)
-
-	wavefunction = hydrogen_wavefunction(n, l, m)
-	density = probability_density(wavefunction, True, m)
-	percent = 0.01
-	level = percent * np.max(density)
-	mask = density >= level
-
-	print("Deseja plotar a função de onda? (s/n)")
-	if input().lower() == 's':
-		mode = int(input("\nEscolha o modo de plotagem\n1: Scatter 3D\n2: Fatias 2D\n3: Isosuperfície\nDigite o número do modo: "))
-		plot_scalar_func(density, mode=mode, mask=mask)
-	
-	print("Deseja salvar a função de onda em um arquivo? (s/n)")
-	if input().lower() == 's':
-		mode = int(input("\nEscolha o modo\n1: Isossuperfície única\n2: Variação da porcentagem\nDigite o número do modo: "))
-
-		if mode == 1:
-			save_obj(density, f"orbital_n{n}_l{l}_m{m}", level=level)
-		elif mode == 2:
-			for i in range(10):
-				save_obj(density, f"orbital_n{n}_l{l}_m{m}", level=level, mode=mode, percent=percent)
-				percent += 0.1
-				level = percent * np.max(density)
-
-def _main():
-	mascara = threshold_3d(arbitrary_scalar, tol=1e-2)
-	plot_scalar_func(arbitrary_scalar, mode=3, mask=mascara)
-	save_to_file(mascara, "arbitrary_points2")
-	save_obj(arbitrary_scalar, "arbitrary_points2")
-
 def auto_orbitals():
 	for n in range(1, 5):
 		atualiza_espaco(n)
