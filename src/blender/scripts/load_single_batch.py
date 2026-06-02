202c910a723404b8be486b19e51f48a0db6330b2
Thiago Oliveira dos Santos <toszerodois@gmail.com>
2026-06-02 17:28:22 -0300

    Arruma o esqueleto dos scripts

--- src/blender/scripts/load_single_batch.py
+++ src/blender/scripts/load_single_batch.py
@@ -1,4 +1,12 @@
+from pathlib import Path
+import sys
 import bpy
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+if str(PROJECT_ROOT) not in sys.path:
+	sys.path.append(str(PROJECT_ROOT))
+
+import src.blender as bl
 
 def main():
 	pass
